dc81e8d24ead83be3da414a058c2e7935e24d908
shubham <shubhamjain839@gmail.com>
2020-04-21 14:52:39 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 //connect to daatabase
 connectDB();
 
@@ -13,7 +13,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-app.get('/', (req, res) => res.send('API Running'));
+//serve static assests in production
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/buid'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
