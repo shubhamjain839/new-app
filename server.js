5a24e04249b71eb79867c1529857f15bc989be78
shubhamjain839 <shubhamjain839@gmail.com>
2020-04-21 15:11:15 +0530

    dep

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 //serve static assests in production
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/buid'));
+  app.use(express.static('client/bulid'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
