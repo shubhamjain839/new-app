6524c49c6d73983eefb7f93f70f64ef531b3245e
shubhamjain839 <shubhamjain839@gmail.com>
2020-04-21 15:23:24 +0530

    minor change

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 //serve static assests in production
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/bulid'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
